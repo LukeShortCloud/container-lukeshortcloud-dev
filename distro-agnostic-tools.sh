c046650c8cf3a40cb258cc77e2e31e1f35fe409f
Luke Short <ekultails@gmail.com>
2025-12-09 11:29:04 -0700

    feat(tools): dynamically find latest version
    for 'golangci-lint', 'vault', 'kind', 'kn', 'kp', 'krew', and 'yq'.
    
    Resolves #18

--- distro-agnostic-tools.sh
+++ distro-agnostic-tools.sh
@@ -14,12 +14,14 @@ rm -r -f ./awscliv2.zip ./aws
 aws --version
 
 # golangci-lint.
-export GOLANGCI_LINT_VER="1.62.0"
+# Example tag name: v2.7.2
+export GOLANGCI_LINT_VER=$(curl --silent https://api.github.com/repos/golangci/golangci-lint/releases/latest | jq -r '.tag_name' | sed 's/v//g')
 curl -sSfL "https://raw.githubusercontent.com/golangci/golangci-lint/v${GOLANGCI_LINT_VER}/install.sh" | sh -s -- -b /usr/local/bin "v${GOLANGCI_LINT_VER}"
 golangci-lint --version
 
 # HashiCorp Vault.
-export VAULT_VER="1.18.1"
+# Example tag name: v1.21.1
+export VAULT_VER=$(curl --silent https://api.github.com/repos/hashicorp/vault/releases/latest | jq -r '.tag_name' | sed 's/v//g')
 wget "https://releases.hashicorp.com/vault/${VAULT_VER}/vault_${VAULT_VER}_linux_amd64.zip"
 unzip vault_${VAULT_VER}_linux_amd64.zip
 sudo mv ./vault /usr/local/bin/
@@ -38,7 +40,8 @@ ytt --version
 wget -O- https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3 | bash
 helm version
 ## kind.
-export KIND_VER="v0.25.0"
+## Example tag name: v0.30.0
+export KIND_VER=$(curl --silent https://api.github.com/repos/kubernetes-sigs/kind/releases/latest | jq -r '.tag_name')
 wget https://kind.sigs.k8s.io/dl/${KIND_VER}/kind-linux-amd64 -O /usr/local/bin/kind
 chmod +x /usr/local/bin/kind
 kind version
@@ -50,17 +53,20 @@ wget https://dl.k8s.io/release/v1.31.2/bin/linux/amd64/kubectl -O /usr/local/bin
 ln -s /usr/local/bin/kubectl-1.28 /usr/local/bin/kubectl
 kubectl version --client
 ## Knative client.
-export KNATIVE_CLIENT_VER="1.16.0"
+## Example tag name: knative-v1.20.0
+export KNATIVE_CLIENT_VER=$(curl --silent https://api.github.com/repos/knative/client/releases/latest | jq -r '.tag_name' | cut -d v -f3)
 wget "https://github.com/knative/client/releases/download/knative-v${KNATIVE_CLIENT_VER}/kn-linux-amd64" -O /usr/local/bin/kn
 chmod +x /usr/local/bin/kn
 kn version
 ## kpack-cli ('kp' command).
-export KPACK_CLI_VER="0.13.0"
+## Example tag name: v0.13.0
+export KPACK_CLI_VER=$(curl --silent https://api.github.com/repos/buildpacks-community/kpack-cli/releases/latest | jq -r '.tag_name' | sed 's/v//g')
 wget https://github.com/buildpacks-community/kpack-cli/releases/download/v${KPACK_CLI_VER}/kp-linux-amd64-${KPACK_CLI_VER} -O /usr/local/bin/kp
 chmod +x /usr/local/bin/kp
 kp version
 ## Krew.
-export KREW_VER="0.4.4"
+## Example tag name: v0.4.5
+export KREW_VER=$(curl --silent https://api.github.com/repos/kubernetes-sigs/krew/releases/latest | jq -r '.tag_name' | sed 's/v//g')
 wget "https://github.com/kubernetes-sigs/krew/releases/download/v${KREW_VER}/krew-linux_amd64.tar.gz"
 tar -xvf krew-linux_amd64.tar.gz ./krew-linux_amd64
 mv ./krew-linux_amd64 /usr/local/bin/krew
@@ -88,7 +94,8 @@ curl -sSf https://sh.rustup.rs | bash -s -- -y
 /usr/local/cargo/bin/cargo version
 
 # yq for querying JSON, XML, and YAML.
-export YQ_VER="v4.44.5"
+# Example tag name: v4.49.2
+export YQ_VER=$(curl --silent https://api.github.com/repos/mikefarah/yq/releases/latest | jq -r '.tag_name')
 wget https://github.com/mikefarah/yq/releases/download/${YQ_VER}/yq_linux_amd64 -O /usr/local/bin/yq
 chmod +x /usr/local/bin/yq
 yq --version
